04c109f0e0b812c7529e9ece015dae96cea8d2c2
Heron Cheng <u101003418@cmu.edu.tw>
2022-07-04 16:26:45 +0800

    toggle between dark mode and sun mode

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
+import { AuthContext } from "../../index";
 
 function App() {
+  const { dark } = useContext( AuthContext );
+  const [ isdark, setIsDark ] = dark;
+  const switchMode=()=>{
+    setIsDark(!isdark);
+      document.getElementById( "app" ).classList.toggle( "dark-mode" );
+  };
   return (
-    <div className="app">
+    <div className="app" id="app">
       <div className="level">
         <div>
           <h1 className="title">Dark Mode Challenge</h1>
         </div>
 
         {/* --The button that should toggle dark mode-- */}
-        <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+        <button className="app__dark-mode-btn icon level-right" onClick={switchMode}>
+          {
+            dark? <FontAwesomeIcon icon={faSun} color="#FFA500"/>:<FontAwesomeIcon icon={faMoon}/>         
+          }
         </button>
 
       </div>
--- dark-mode/src/routes/index.js
+++ dark-mode/src/routes/index.js
@@ -1,5 +1,20 @@
-import Home from './App';
+import App from './App';
+import React, {useState} from 'react';
+
+export const AuthContext = React.createContext();
 
 // Use something like react-router-dom to manage multiple pages/routes
+const Home=()=>{
+    const [dark, setDark]=useState(false);
+    return(
+        <AuthContext.Provider
+                value={{
+                    dark : [ dark, setDark ],
+                }}
+            >
+                <App/>
+        </AuthContext.Provider>
+    )
+}
 
 export default Home;
